3ba338b6704cd877febf77ca506b8f0030c415aa
Hoang_Thai <tlee692001@gmail.com>
2023-05-04 16:06:21 +0700

    add UI, components

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<template>
+	<div id="app">
+		<div class="wrapper clearfix">
+			<players v-bind:scorePlayers="scorePlayers" 
+			v-bind:activePlayer="activePlayer"
+			v-bind:curentScore="curentScore"/>
+
+			<controls />
+
+			<dices />
+
+		</div>
+	</div>
+</template>
+
+<script>
+import Players from './components/Players.vue';
+import Controls from './components/Controls.vue';
+import Dices from './components/Dices.vue';
+export default {
+	name: 'app',
+	data() {
+		return {
+			isPlaying: false,
+			activePlayer:0,
+			scorePlayers: [18,26],
+			curentScore: 10
+		}
+	},
+	methods: {
+
+	},
+	components: {
+		Players,
+		Controls,
+		Dices
+	}
+}
+</script>
+
+<style>
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+
+}
+
+.clearfix::after {
+	content: "";
+	display: table;
+	clear: both;
+}
+
+body {
+	background-image: linear-gradient(rgba(62, 20, 20, 0.4), rgba(62, 20, 20, 0.4)), url('/public/assets/back.jpg');
+	background-size: cover;
+	background-position: center;
+	font-family: Lato;
+	font-weight: 300;
+	position: relative;
+	height: 100vh;
+	color: #555;
+}
+
+.wrapper {
+	width: 1000px;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	background-color: #fff;
+	box-shadow: 0px 10px 50px rgba(0, 0, 0, 0.3);
+	overflow: hidden;
+}
+</style>
